bbc2c54ceac8a2777140a0b274766cd77965f764
wangning007 <18254250119@163.com>
2018-11-28 17:48:43 +0800

    中国呈现边框颜色修改

--- SOTN/gis3/businessFault/e_china.js
+++ SOTN/gis3/businessFault/e_china.js
@@ -44,8 +44,8 @@
             };
             // 点击事件：线条高亮，两端闪烁
             $(".port").click(function () {
-                $.fn.ChinaModule.methods.linesEventTrigger({ id: "123", chart: chart })
-            })
+
+            });
         },
         renderData: function (chart) {
             var points = $("body").GeoUtils('getEffectScatters');
@@ -160,50 +160,6 @@
                     chart.setOption(opt);
                 } else {
                     chart.setOption(old_opt);
-                }
-            });
-        },
-        linesEventTrigger: function (param) {
-            var oid = param.id;
-            var chart = param.chart;
-            var option = chart.getOption();
-            var series = option.series;
-            // map  effectScatter  lines
-
-            series.forEach((element,index) => {
-                if (element.type == 'lines') {
-                    // 在线条里寻找 需要高亮的那条线，拿到端点数据
-                    var linesData = element.data;
-                    linesData.forEach((ele, i) => {
-                        var linesAggr = ele.data.aggr;
-                        linesAggr.forEach(ele => {
-                            if(oid == ele.oid) {
-                                var planePath = 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z';
-                                series.push({
-                                    type: 'lines',
-                                    name: 'lines专线',
-                                    effect: {
-                                        show: true,
-                                        period: 4,
-                                        // 迁移
-                                        symbol: planePath,
-                                        symbolSize: 15
-                                    },
-                                    data: [{
-                                        name: linesData[i].oname,
-                                        coords: linesData[i].coords,
-                                        lineStyle: {color: "blue",width:2,curveness: 0.2}
-                                    }]
-                                },{
-                                    type: 'effectScatter',
-                                    name: 'effectScatter',
-                                    // itemStyle: 
-                                    data: linesData[i].coords
-                                });
-                                chart.setOption(option)
-                            }
-                        });
-                    });
                 }
             });
         }
