beff6a3e89ae60f62217339372a4934b4b22152c
lisuhao <lisuhao@inspur.com>
2019-05-25 14:42:15 +0800

    xxxxxx
    Code Source From: Self Code
    # Self Code / OpenSource / 3rd party Code / Other
    Description:
    Jira:
    市场项目编号（名称）:

--- SOTN-front/src/container/KeySearch.js
+++ SOTN-front/src/container/KeySearch.js
@@ -5,14 +5,68 @@ import React,{ Component } from 'react';
 import esriLoader from 'esri-loader';
 import Iframe from './Iframe';
 import { formatNumber, baseStaticUrl, msToHour } from '../util/CommonUtils';
+import { Select, Button } from 'antd';
+import '../assets/css/search/search.scss';
+import { postAxios,getAxios, postAxiosBodyAndQuery } from '../axios/mainAxios';
 
+const Option = Select.Option;
 
 export default class KeySearch extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      resultData: [],
+      value: undefined,
+      checkedFlag: 0,
+      placeholder:'请输入站点..'
+    };
   }
-  componentDidMount(){
+  handleSearch = value => {
+    // var data = [];
+    var param = {
+      nameLike: value
+    };
+    if (this.state.checkedFlag === 0) {
+      postAxios('/api/network/sites?pageSize=10&currentPage=1', param, data =>{
+        this.setState({ resultData: data.results });
+      });
+    } else if (this.state.checkedFlag === 1) {
+      postAxios('/api/network/elements?pageSize=10&currentPage=1', param, data =>{
+        this.setState({ resultData: data.results });
+      });
+    } else if (this.state.checkedFlag === 2) {
+      getAxios('/api/customers?pageSize=10&currentPage=1&nameLike='+ param.nameLike, data =>{
+        this.setState({ resultData: data.results });
+      });
+    } else if (this.state.checkedFlag === 3) {
+      getAxios('/api/leased_lines?pageSize=10&currentPage=1&nameLike='+ param.nameLike, data =>{
+        this.setState({ resultData: data.results });
+      });
+    }
 
+  };
+  handleChange = value => {
+    this.setState({ value });
+  };
+  componentDidMount(){
+    this.handleSearchXX();
+  }
+  // getType = () => {
+  //   postAxios('api/network/stats/site/type', this.getFilterParams(), data =>
+  //     this.setState({ siteTypeArr: objectEchartsArray(data.values) })
+  //   );
+  // };
+  handleSearchXX =()=>{
+    // getAxios('api/leased_lines?nameLike="专线"', data =>{
+    //   alert(123);
+    //   console.error('dataxxxx',data);
+    // });
+    var param = {
+      nameLike: '测试'
+    };
+    postAxios('/api/network/sites', param, data =>
+      console.error('site', data)
+    );
   }
   initMap(){
 
@@ -49,7 +103,40 @@ export default class KeySearch extends Component {
       map.centerAt(point);
     });
   }
+  handleTabChecked =(param)=>{
+    this.handleSearchXX();
+    this.setState({
+      checkedFlag: param,
+      resultData: []
+    });
+    // this.setState({ resultData: [] });
+    switch(param) {
+    case 0 :
+      this.setState({
+        placeholder: '请输入站点..'
+      });
+      break;
+    case 1:
+      this.setState({
+        placeholder: '请输入网元..'
+      });
+      break;
+    case 2:
+      this.setState({
+        placeholder: '请输入客户..'
+      });
+      break;
+    case 3:
+      this.setState({
+        placeholder: '请输入专线..'
+      });
+      break;
+    default:
+      break;
+    }
+  }
   render() {
+    const options = this.state.resultData.map(d => <Option key={d.id}>{d.name}</Option>);
     return (
       <div style={{width:'100%',height:800}}>
         <Iframe
@@ -60,6 +147,32 @@ export default class KeySearch extends Component {
             clearWarnOtnNetworkFault: this.clearWarnOtnNetworkFault
           }}
         />
+        <div
+          className="seach-modal">
+          <div id="search-bd" className="search-bd">
+            <ul>
+              <li onClick = {this.handleTabChecked.bind(this,0)} className={this.state.checkedFlag === 0 ? 'selected':''}>站点</li>
+              <li onClick = {this.handleTabChecked.bind(this,1)} className={this.state.checkedFlag === 1 ? 'selected':''}>网元</li>
+              <li onClick = {this.handleTabChecked.bind(this,2)} className={this.state.checkedFlag === 2 ? 'selected':''}>客户</li>
+              <li onClick = {this.handleTabChecked.bind(this,3)} className={this.state.checkedFlag === 3 ? 'selected':''}>专线</li>
+            </ul>
+          </div>
+          <Select
+            className="search-select"
+            showSearch
+            value={this.state.value}
+            placeholder={this.state.placeholder}
+            style={this.props.style}
+            defaultActiveFirstOption={false}
+            showArrow={false}
+            filterOption={false}
+            onSearch={this.handleSearch}
+            onChange={this.handleChange}
+            notFoundContent={null}>
+            {options}
+          </Select>
+          <Button type="primary" icon="search"/>
+        </div>
       </div>
     );
   }
---
+++ SOTN/gis3/key_search/map.js
@@ -0,0 +1,17 @@
+/**
+ * @author wang.ning
+ */
+require(["./MapManager.js"],function(MapManager){
+    if(!Global.mapGlobal) Global.mapGlobal = {};
+
+    Global.mapGlobal.clickGraphic = {}
+    Global.mapGlobal.clickGraphic.gra = null;    //单击选中的graphic,用来做图标切换
+    Global.mapGlobal.clickGraphic.sym = null;    //单击选中graphic的symbol
+
+    Global.mapGlobal.instance = {};     //instances
+    Global.mapGlobal.instance.mapManager = new MapManager();
+
+    Global.datas = {};
+    Global.datas.warningDatas = null;    //存放告警数据
+
+});
